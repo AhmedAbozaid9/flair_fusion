c88f3bcbbfb433bdef3f310dcd71898604f8e36e
Ahmed Abozaid <ahmedabozaid493@gmail.com>
2024-01-05 14:39:14 +0200

    handles when the user enters cart and wishlist when not signed in

---
+++ src/middleware.js
@@ -0,0 +1,17 @@
+import { getToken } from "next-auth/jwt";
+import { NextResponse } from "next/server";
+export async function middleware(req) {
+  const pathname = req.nextUrl.pathname;
+  const protectedPaths = ["/wishlist", "/cart"];
+  const isPathProtected = protectedPaths?.some((path) => pathname == path);
+  const res = NextResponse.next();
+  if (isPathProtected) {
+    const token = await getToken({ req });
+    if (!token) {
+      const url = new URL(`/sign-in`, req.url);
+      url.searchParams.set("callbackUrl", pathname);
+      return NextResponse.redirect(url);
+    }
+  }
+  return res;
+}
